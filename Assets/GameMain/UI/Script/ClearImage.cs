9bb0427daeb8d17316c867984e1e7e2ef5c9add7
kato <mianida@ezweb.ne.jp>
2015-11-25 14:25:16 +0900

    クリア画像を表示しました。

---
+++ Assets/GameMain/UI/Script/ClearImage.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ClearImage : MonoBehaviour {
+
+    GameObject playercamera;
+    PlayerCamera camera;
+	// Use this for initialization
+	void Start () {
+        GetComponent<Image>().enabled = false;
+        playercamera = GameObject.Find("Camera");
+        camera = playercamera.GetComponent<PlayerCamera>();
+	}
+    
+	
+	// Update is called once per frame
+	void Update () {
+        if (camera.clearFlag)
+        {
+            GetComponent<Image>().enabled = true;
+        }
+	}
+}
