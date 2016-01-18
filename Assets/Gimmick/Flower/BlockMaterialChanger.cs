5af12664d4274f83bddaa7e8adb9eda7cb74a02f
SyuheiMatsumura <s212.1995@gmail.com>
2016-01-18 13:47:46 +0900

    マテリアルチェンジャーでエフェクトも変更できるように変更

--- Assets/Gimmick/Flower/BlockMaterialChanger.cs
+++ Assets/Gimmick/Flower/BlockMaterialChanger.cs
@@ -5,6 +5,8 @@ public class BlockMaterialChanger : MonoBehaviour {
 
 
     public Material[] matelials;
+    public GameObject Effect;
+    public GameObject[] HoleEffect;
 	// Use this for initialization
 	void Start () {
 	    
@@ -19,5 +21,16 @@ public class BlockMaterialChanger : MonoBehaviour {
     public void MatelialChange(int Number)
     {
         GetComponent<MeshRenderer>().material = matelials[Number-1];
+    }
+
+
+    public void EffectChange(int Number)
+    {
+        // タッチした画面座標からワールド座標へ変換
+        Vector3 pos = new Vector3(0.0f, 0.0f, 0.0f);
+
+        pos = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y+0.5f, gameObject.transform.position.z);
+
+        Effect = (GameObject)Instantiate(HoleEffect[Number-1], pos, Quaternion.identity);
     }
 }
